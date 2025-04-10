71ac3f1dd484884e90191bf8f41e0345edd326af
Blindless1 <owenstutelberg@gmail.com>
2025-04-10 12:09:11 -0500

    Added directionality to the AutoDriveCommand

--- src/main/java/frc/robot/commands/AutoDriveCommand.java
+++ src/main/java/frc/robot/commands/AutoDriveCommand.java
@@ -8,26 +8,28 @@ public class AutoDriveCommand extends Command {
     // Initialize the drive subsystem and distance 
     private DriveSubsystem driveSubsystem;
     private double distance;
+    private double direction;
 
     /**
      * Automatically drives the robot for a certain distance
      * @param driveSubsystem Subsystem needed to drive
-     * @param distance Distance in inches you want the bot to drive before stopping
+     * @param distance Inches you want to drive forwards before stopping
      */
     public AutoDriveCommand(DriveSubsystem driveSubsystem, double distance) {
         this.driveSubsystem = driveSubsystem;
         this.distance = distance / Constants.WHEEL_CIRCUMFERENCE; // Convert inches to wheel rotations
+        this.direction = distance / Math.abs(distance); // -1 if driving backwards, 1 if driving forwards
         
         addRequirements(driveSubsystem);
     }
 
     public void execute() {
+        // If the robot is past the distance, stop; else, drive.
         if (distance >= driveSubsystem.getAverageDriveDistance()) {
             driveSubsystem.drive(0, 0);
         } else {
-            driveSubsystem.drive(Constants.DRIVE_SPEED, 0);
+            driveSubsystem.drive(direction * Constants.DRIVE_SPEED, 0);
         }
-
     }
 
     public void end(boolean interrupted) {
