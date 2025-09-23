12149ff267d29495483a0392d467fd05ad32be27
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2025-09-23 16:38:35 -0500

    s

--- src/main/java/frc/robot/subsystems/JeremeySubystem.java
+++ src/main/java/frc/robot/subsystems/JeremeySubystem.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase; 
 
 public class JeremeySubystem extends SubsystemBase{
     
