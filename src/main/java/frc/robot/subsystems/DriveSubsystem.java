78d2ec4db19691effd5e1a548fddda6626ca1c30
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2025-10-07 16:13:53 -0500

    hi

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase{
+
+    private SparkMax jereyLeftFront = new SparkMax(1, MotorType.kBrushless);
+    private SparkMax jereyLeftBack = new SparkMax(2, MotorType.kBrushless);
+    private SparkMax jereyRightFront = new SparkMax(3, MotorType.kBrushless);
+    private SparkMax JereyRightBack = new SparkMax(4, MotorType.kBrushless);
+
+    private SparkMaxConfig leftConfig = new SparkMaxConfig();
+    private SparkMaxConfig rightConfig = new SparkMaxConfig();
+
+    
+    public DriveSubsystem (){
+        leftConfig.follow(1);
+        jereyLeftBack.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        rightConfig.follow(3);
+        JereyRightBack.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+
+    }
+}
+ 
