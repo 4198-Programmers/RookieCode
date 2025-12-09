f79b6284359a85028c5fe78462d304cbf5a3ec38
pipercycle-wq <pipercycle@gmail.com>
2025-12-09 15:24:52 -0800

    tyler is a pretty cool handsome guy

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -1,28 +1,22 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class MotorSubsystem extends SubsystemBase{
+    
+private SparkMax motor = new SparkMax(14, MotorType.kBrushless);
 
-    private SparkMax motorOne = new SparkMax(14, MotorType.kBrushless);
 
+public MotorSubsystem() {
 
-
-    public MotorSubsystem() {
+public void runMotor() {
 
     
-        
-    }
+}
 
-    public void runMotor(double speed) {
-
-        motorOne.set(speed);
-
-    }
-
-    //run motor
+}
 
 }
