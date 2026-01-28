bf2885591a08050c001a1fd3bda1a2f39b11b156
Spid3r52 <trashpanda259@gmail.com>
2026-01-27 18:24:30 -0600

    drive done
    for chairbot

---
+++ src/main/java/frc/robot/subsystems/ChairBotDrive.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ChairBotDrive extends SubsystemBase{
+
+    private 
+    public ChairBotDrive () {
+
+        public jerrySpark() = new sparkMax 
+
+        /*'[drive= workep
+        jiggle physics = yes'*/
+    }
+
+}
