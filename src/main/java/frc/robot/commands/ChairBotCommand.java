963a1fb573195f50810660bbb3e4213a23bda17a
Spid3r52 <trashpanda259@gmail.com>
2026-02-04 09:05:11 -0600

    Create ChairBotCommand.java

---
+++ src/main/java/frc/robot/commands/ChairBotCommand.java
@@ -0,0 +1,9 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ChairBotDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ChairBotCommand extends Command {
+    private final ChairBotDrive comm;
+}
